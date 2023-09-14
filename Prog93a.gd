7956e475e3bf9e03ad1107d1a76f24df1f3c6ab5
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2023-09-14 12:31:25 -0500

    update 93a

--- Prog93a.gd
+++ Prog93a.gd
@@ -9,15 +9,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func round_to_dec(num, digit):
+	return round(num * pow(10.0, digit)) / pow(10.0, digit)
 
 
 func _on_button_pressed():
 	var kwatts = int($LineEdit.text)
-	var brate = round(kwatts * 0.0475)
-	var scharge = round(brate * 0.1)
-	var ctax = round(brate * 0.03)
-	var total = round(brate + scharge + ctax)
-	var ltotal = round(total * 1.04)
+	var brate = round_to_dec(kwatts * 0.0475, 2)
+	var scharge = round_to_dec(brate * 0.1, 2)
+	var ctax = round_to_dec(brate * 0.03, 2)
+	var total = round_to_dec(brate + scharge + ctax, 2)
+	var ltotal = round_to_dec(total * 1.04, 2)
 	$lblOut.text = "Base Rate: $" + str(brate) + "\nSurcharge: $" + \
 	str(scharge) + "\nCity Tax: $" + str(ctax) + "\nPay this amount: $" + \
 	str(total) + "\nPay this if late: $" + str(ltotal)
