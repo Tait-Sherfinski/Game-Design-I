40c95a70a217e1182adfb5adcf495acb9b218b71
Tait-Sherfinski <Tsherfin@gmail.com>
2023-09-22 12:47:15 -0500

    update 9/22

--- LP4-3.gd
+++ LP4-3.gd
@@ -15,18 +15,21 @@ func _on_button_pressed():
 	var eggs = int($txtEggs.text)
 	var price = 0.0
 	var cost = 0.0
-	if eggs < 48:
-		price = 0.50
+	var dozens = floor(eggs / 12)
+	var extrap = eggs % 12
+	
+	if eggs > 0 and eggs < 48:
+		price = 0.50 * dozens
 	elif eggs >= 48 and eggs < 72:
-		price = 0.45
+		price = 0.45 * dozens
 	elif eggs >= 72 and eggs < 132:
-		price = 0.40
+		price = 0.40 * dozens
 	elif eggs >= 132:
-		price = 0.35
-		return
+		price = 0.35 * dozens
 		
-	cost = price * eggs
-	$Label2.text = "The bill is $" + str(cost)
+	cost = price
+	
+	$Label2.text = "The price is $" + str(price)
 
 
 func _on_button_2_pressed():
