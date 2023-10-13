ec3c8a4857e285ff9b165c894cf9e247275adf09
Tait-Sherfinski <Tsherfin@gmail.com>
2023-10-13 12:20:44 -0500

    update 10/13

--- Platformer.gd
+++ Platformer.gd
@@ -6,15 +6,8 @@ func _ready():
 	$World/CollisionPolygon2D/Polygon2D.polygon = $World/CollisionPolygon2D.polygon
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_interact_body_entered(body):
-	pass
-
 
 func _on_exit_body_entered(body):
 	if body.name == "Keyboard_player":
 		OS.alert("You won!")
+		body.queue_free()
