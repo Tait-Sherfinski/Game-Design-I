577df0f0a53c8d5ebf20eb4d93bddd3c632d8d03
Tait-Sherfinski <Tsherfin@gmail.com>
2023-10-20 12:48:29 -0500

    update 10/20

--- kbdenemy.gd
+++ kbdenemy.gd
@@ -5,6 +5,7 @@ var direction = -1
 
 func _ready():
 	$Timer.start()
+	update_animation()
 
 func _physics_process(delta):
 	self.linear_velocity.x = speed * direction
@@ -14,6 +15,10 @@ func _on_timer_timeout():
 	
 func _on_body_entered(body):
 	if body.name == "Keyboard_player":
+		OS.alert("You Died")
 		body.queue_free()
-		OS.alert("You Died!")
 		get_tree().reload_current_scene()
+		
+@onready var anim = $AnimatedSprite2D
+func update_animation():
+	anim.play("default")
