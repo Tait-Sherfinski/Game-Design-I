1a85a2efd11c39067e6e2c296fabf186079629ff
Tait-Sherfinski <Tsherfin@gmail.com>
2023-11-02 12:46:59 -0500

    update

--- phys_test.gd
+++ phys_test.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Panel/HSlider.value = $someShape.gravity_scale
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,22 +12,24 @@ func _process(delta):
 	
 	
 func _input(event):
-	if event is InputEventMouseButton and event.pressed:
-		var nsquare = preload("res://some_shape.tscn").instantiate()
-		nsquare.position = event.position
-		add_child(nsquare)
+	pass
 
 
 func _on_h_slider_value_changed(value):
-	for child in get_children():
-		if child is RigidBody2D:
-			child.gravity_scale = value
+	pass
 
 
 func _on_button_pressed():
-	$someShape.gravity_scale = 1
-	$Panel/HSlider.value = $someShape.gravity_scale
-	$someShape.visible = false
 	for child in get_children():
 		if child is RigidBody2D:
-			remove_child(child)
+			child.apply_central_impulse(Vector2.RIGHT * 500)
+
+
+func _on_h_slider_2_value_changed(value):
+	pass
+
+
+func _on_button_2_pressed():
+	for child in get_children():
+		if child is RigidBody2D:
+			child.apply_central_impulse(Vector2.LEFT * 500)
