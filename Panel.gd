0302deee8fa473e8e08d65989645cc043c322b70
Tait-Sherfinski <Tsherfin@gmail.com>
2023-11-06 12:48:57 -0600

    update 11/6

---
+++ Panel.gd
@@ -0,0 +1,15 @@
+extends Panel
+
+
+func _ready():
+	self.process_mode = Node.PROCESS_MODE_ALWAYS	
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_resume_pressed():
+	hide()
+	get_tree().paused = false
