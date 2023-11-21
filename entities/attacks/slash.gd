a48b175ecb1cd34569ffd140619301c96575dbc6
Tait-Sherfinski <Tsherfin@gmail.com>
2023-11-21 12:48:31 -0600

    update

--- entities/attacks/slash.gd
+++ entities/attacks/slash.gd
@@ -1,11 +1,17 @@
 extends Area2D
 
+var damage = 10
+var knockback = 64
+var anim_life = 0.2
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	anim_life -= delta
+	if anim_life < 0:
+		queue_free()
+		return
+		
+	for enemy in get_tree().get_nodes_in_group("Enemy"):
+		if overlaps_body(enemy):
+			enemy.take_damage(damage, self)
+			enemy.inertia = (enemy.global_position -
+			self.global_position.normalized() * knockback)
